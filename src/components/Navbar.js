ba164f888d4019292c9406ddbab9ebdd0863cad1
apollo254 <shadrackapollo@gmail.com>
2024-01-29 21:53:36 +0000

    added logo icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import '../styles/Navbar.css';
+import logo from '../assets/Exclude.svg';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Nile cash</div>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <span className="logo-text">Nile Cash</span>
+      </div>
       <div className="menu">
         <a href="#why-us">Why Us</a>
         <a href="#services">Services</a>
         <a href="#process">Our Process</a>
         <a href="#loans">Loans</a>
       </div>
-      <button className="sign-in-button">Sign in</button>
+      <button className="sign-in-button">Request Loan</button>
     </nav>
   );
 };
